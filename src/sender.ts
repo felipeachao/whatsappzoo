a856ef1a4479b1efbe60c375a75a7eeab5146346
joao.achao <joao.achao@conecthus.org.br>
2022-07-29 02:52:50 -0400

    feat:  bot sending message

---
+++ src/sender.ts
@@ -0,0 +1,60 @@
+import { start } from "repl"
+import { create, Whatsapp, Message, SocketState } from "venom-bot"
+
+export type QRCode = {
+    base64Qr: string
+    attempts: number
+}
+
+class Sender {
+    private client: Whatsapp
+    private connected: boolean
+    private qr: QRCode
+
+    get isConnected() : boolean {
+        return this.connected
+    }
+
+    get qrCode(): QRCode {
+        return this.qr
+    }
+
+    constructor(){
+        this.initialize()
+    }
+
+    async sendText(to: string, body: string) {
+                
+        await this.client.sendText(to, body)
+
+    }
+
+    private initialize() {
+        const qr = (base64Qr: string, asciiQR:string, attempts: number) => {
+            this.qr = {base64Qr, attempts}
+        }
+
+        const status = (statusSession: string) => {
+      // return isLogged || notLogged || browserClose || qrReadSuccess || qrReadFail || autocloseCalled || desconnectedMobile || deleteToken || 
+      // chatsAvailable || deviceNotConnected || serverWssNotConnected || noOpenBrowser || initBrowser || openBrowser || connectBrowserWs || 
+      // initWhatsapp || erroPageWhatsapp || successPageWhatsapp || waitForLogin || waitChat || successChat
+      // Create session wss return "serverClose" case server for close
+            this.connected = ["isLogged", "qrReadSucess", "chatsAvailable"].includes(statusSession)
+        }
+
+        const start = (client: Whatsapp) => {
+            this.client = client
+
+            client.onStateChange((state) => {
+                this.connected = state === SocketState.CONNECTED
+            })
+
+        }
+         
+        create('ws-sender-dev',qr)
+            .then((client) => start(client))
+            .catch((error) => console.error(error))
+    }
+}
+
+export default Sender
